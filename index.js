f88afa8af50fa19e7a486a45d6ab80af278a8f11
Taiwen <kimdaxterkid@gmail.com>
2020-01-14 10:45:03 -0800

    Update 011420

--- index.js
+++ index.js
@@ -2,18 +2,34 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+//------------------------------------------------------------------------------
+//  File System
+//------------------------------------------------------------------------------
+
 fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
     console.log(data);
 });
 
+const data = fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+//------------------------------------------------------------------------------
+//  Server
+//------------------------------------------------------------------------------
 const server = http.createServer((req, res) => {
     console.log(req.url);
     const pathName = req.url;
     if (pathName === '/' || pathName === '/overview') {
         res.end('This is the OVERVIEW page');
-    } else if (pathName === '/product') {
+    }
+    else if (pathName === '/product') {
         res.end('This is the PRODUCT page');
-    } else {
+    }
+    else if (pathName === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json' });
+        res.end(data);
+    }
+    else {
         res.writeHead(404, {
             'Content-type': 'text/html',
             'my-own-header': 'hello-world'
