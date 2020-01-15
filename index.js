674dfadafb3aa31d2342958379e33dbad762c4ed
Taiwen <kimdaxterkid@gmail.com>
2020-01-15 08:25:40 -0800

    Update 011520

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ const url = require('url');
 //------------------------------------------------------------------------------
 //  File System
 //------------------------------------------------------------------------------
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    if (!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+    }
+    return output;
+};
+const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
-    console.log(data);
-});
-
-const data = fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 //------------------------------------------------------------------------------
@@ -20,7 +33,10 @@ const server = http.createServer((req, res) => {
     console.log(req.url);
     const pathName = req.url;
     if (pathName === '/' || pathName === '/overview') {
-        res.end('This is the OVERVIEW page');
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.end(output);
     }
     else if (pathName === '/product') {
         res.end('This is the PRODUCT page');
@@ -39,6 +55,6 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to reqests on port 8000');
+    console.log('Listening to requests on port 8000');
 });
 
